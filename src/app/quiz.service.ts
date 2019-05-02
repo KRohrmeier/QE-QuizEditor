e852194f55a5d9c02acd982d2573fbb8d86bb1ff
Kat Rohrmeier <39012778+KRohrmeier@users.noreply.github.com>
2019-05-02 12:39:36 -0500

    Update quiz.service w/save() ...
    ... and remove old/refactored code

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class QuizService {
-
-  constructor(private builtInAngularHttpClient: HttpClient) { }
+  constructor(private builtInAngularHttpClient: HttpClient) {}
 
   getQuizzes() {
+    return this.builtInAngularHttpClient.get(
+      "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
+    );
 
-    return this.builtInAngularHttpClient.get("https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz");
-
-    // Mocking up the data we will ultimately get from a 
+    // Mocking up the data we will ultimately get from a
     // REST endpoint, or web service call...
     // return [
     //   { name: 'Quiz 1', numberQuestions: 3 }
@@ -24,11 +24,21 @@ export class QuizService {
     // }));
   }
 
-  getNumberPromise(doYouWantMeToSucceed: boolean) {
-    let p = new Promise<number> (
-      (resolve, reject) => doYouWantMeToSucceed ? resolve(42) : reject("You got problems!")
-    );
+  saveQuizzes(changedQuizzes: any[], newQuizzes: any[] = []) {
+    let h = new HttpHeaders({
+      "Content-Type": "application/json",
+      "X-Sas-Token": "sig=K2WE6NQPtyoV6ke5hwPEaEaW52fgvyFWUeCEdPJls1s"
+    });
 
-    return p;
+    return this.builtInAngularHttpClient.post(
+      "https://modern-js.azurewebsites.net/save-quizzes-proxy",
+      JSON.stringify({
+        changedQuizzes: changedQuizzes,
+        newQuizzes: newQuizzes
+      }),
+      {
+        headers: h
+      }
+    );
   }
 }
